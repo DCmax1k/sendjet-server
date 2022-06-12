c08cce4c8d6c63f403e4224fd591d09fc5714a24
Dylan Caldwell <dylan.caldwell35@gmail.com>
2022-06-11 22:58:33 -0400

    async await error fixed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -20,12 +20,12 @@ router.post('/', authToken, async (req, res) => {
         const user = await User.findById(req.userId);
         if (!user) return res.status(200).json({ status: 'error', message: 'User does not exist' });
 
-        const conversations = user.conversations.map(async convo => {
+        const conversations = await Promise.all(user.conversations.map(async convo => {
             return await Conversation.findById(convo);
-        });
-        const friends = user.friends.map(async friend => {
+        }));
+        const friends = await Promise.all(user.friends.map(async friend => {
             return await User.findById(friend);
-        });
+        }));
 
         const jwt_token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
         res.cookie('auth-token', jwt_token, { httpOnly: true, expires: new Date(Date.now() + 20 * 365 * 24 * 60 * 60 * 1000) });
