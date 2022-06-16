085fc92a300876ce55ade34d7c01287b4b7a061e
Dylan Caldwell <dylan.caldwell35@gmail.com>
2022-06-16 17:08:32 -0400

    testing socketio

--- socket.js
+++ socket.js
@@ -5,15 +5,37 @@ const server = require('./server');
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+const usersOnline = []; // Array of user objects with socketID and userID
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('testing', (data) => {
-        console.log(data);
-    })
+    socket.on('joinUserRoom', ({ id }) => {
+        socket.join(id);
+        usersOnline.push({ socketID: socket.id, userID: id });
+
+        io.emit('currentlyOnline', usersOnline);
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+
+        const user = usersOnline.find(user => user.socketID === socket.id);
+        if (user) usersOnline.splice(usersOnline.map(guy => guy.userID).indexOf(user.userID), 1);
     });
 })
 
