5383df48cc0847510d3eaa61a6d984a126a8ec34
Dylan Caldwell <dylan.caldwell35@gmail.com>
2022-07-09 22:34:04 -0400

    Profile pic upload

--- routes/profile.js
+++ routes/profile.js
@@ -8,6 +8,13 @@ const Conversation = require('../models/Conversation');
 
 const { getStorage, ref, deleteObject, uploadBytes } = require('firebase/storage');
 const storage = getStorage();
+const multer = require("multer");
+const upload = multer({
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+
+  },
+}).single("photo");
 
 router.post('/changefirstname', authToken, async (req, res) => {
     try {
@@ -112,14 +119,13 @@ router.post('/changeusernamecolor', authToken, async (req, res) => {
     }
 });
 
-router.post('/changeprofilepicture', authToken, async (req, res) => {
+router.post('/changeprofilepicture', [authToken, upload], async (req, res) => {
     try {
-        const {bytes} = req.body;
-        console.log(bytes);
+        
 
         const userRef = ref(storage, req.body.userId);
         const imgRef = ref(userRef, `profilePicture`);
-        await uploadBytes(imgRef, bytes);
+        await uploadBytes(imgRef, req.file.buffer);
 
         res.json({
             status: 'success',
